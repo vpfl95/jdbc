5cf1accf5fd2d683f0572f582e56677052bbbcfe
vpfl95 <k_dh610@naver.com>
2023-04-07 17:37:37 +0900

    예외 포함과 스택 트레이스

--- src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
+++ src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
@@ -17,6 +17,16 @@ public class UnCheckedAppTest {
                 .isInstanceOf(Exception.class);
     }
 
+    @Test
+    void printEx(){
+        Controller controller = new Controller();
+        try {
+        controller.request();
+        } catch (Exception e){
+            log.info("ex",e);
+        }
+    }
+
     static class Controller{
         Service service = new Service();
 
