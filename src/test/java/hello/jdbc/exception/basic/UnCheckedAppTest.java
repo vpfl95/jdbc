7302d05e3fe8ca79ffd0260015604d11b29f3bba
vpfl95 <k_dh610@naver.com>
2023-04-07 17:31:51 +0900

    언체크 예외 활용

---
+++ src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
@@ -0,0 +1,67 @@
+package hello.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.net.ConnectException;
+import java.sql.SQLException;
+
+@Slf4j
+public class UnCheckedAppTest {
+
+    @Test
+    void uncehcked(){
+        Controller controller = new Controller();
+        Assertions.assertThatThrownBy(()->controller.request())
+                .isInstanceOf(Exception.class);
+    }
+
+    static class Controller{
+        Service service = new Service();
+
+        public void request(){
+            service.logic();
+        }
+    }
+
+    static class  Service{
+        Repository repository = new Repository();
+        NetworkClient networkClient = new NetworkClient();
+        public void logic(){
+            repository.call();
+            networkClient.call();
+        }
+
+    }
+    static class  NetworkClient{
+        public void call(){
+            throw new RunTimeConnectException("연결실패");
+        }
+    }
+    static class  Repository{
+        public void call() {
+            try {
+                runSQL();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public void runSQL() throws SQLException {
+            throw new SQLException("ex");
+        }
+    }
+
+    static class RunTimeConnectException extends RuntimeException{
+        public RunTimeConnectException(String message) {
+            super(message);
+        }
+    }
+
+    static class RuntimeSQLExeption extends RuntimeException{
+        public RuntimeSQLExeption(Throwable cause) {
+            super(cause);
+        }
+    }
+}
